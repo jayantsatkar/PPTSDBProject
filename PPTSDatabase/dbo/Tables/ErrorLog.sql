468e0c0ef87d2ad3d16f1c515f72755531309626
Jayant Satkar <jayant.satkar@gmail.com>
2025-02-16 15:55:31 +0530

    db COMPATIBILITY CHANGED

--- PPTSDatabase/dbo/Tables/ErrorLog.sql
+++ PPTSDatabase/dbo/Tables/ErrorLog.sql
@@ -3,7 +3,9 @@
     [ERROR_LINE]      INT             NULL,
     [ERROR_NUMBER]    INT             NULL,
     [ERROR_MESSAGE]   NVARCHAR (4000) NULL,
-    [ERROR_TIME]      DATETIME        NOT NULL,
-    [ERROR_TYPE]      CHAR (1)        CONSTRAINT [DF_ErrorLog_ERROR_TYPE] DEFAULT ('E') NOT NULL
+    [ERROR_TIME]      DATETIME        NOT NULL DEFAULT getdate(),
+    [ERROR_TYPE]      CHAR (1)        CONSTRAINT [DF_ErrorLog_ERROR_TYPE] DEFAULT ('E') NOT NULL, 
+    [Id] INT NOT NULL IDENTITY(1,1), 
+    CONSTRAINT [PK_ErrorLog] PRIMARY KEY ([Id])
 );
 
